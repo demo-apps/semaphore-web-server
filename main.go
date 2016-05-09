d2823f81f96fe69219136d53d9ca009f286c2ca3
user <user@localhost>
2016-05-09 13:36:45 +0530

    Store token in cookie. Add a route to serve protected content.

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ func main() {
 	s := gin.Default()
 
 	s.POST("/login", login)
-	s.POST("/logout", logout)
+	s.GET("/logout", logout)
+	s.GET("/protected-content", serveProtectedContent)
 
 	s.Run(":8000")
 }
@@ -27,8 +28,12 @@ func login(c *gin.Context) {
 	password := c.PostForm("password")
 
 	if response := auth.Login(username, password); response.Token != "" {
-		// If authentication succeeds respond with an HTTP success
+		// If authentication succeeds set the cookies and
+		// respond with an HTTP success
 		// status and include the token in the response
+		c.SetCookie("username", username, 3600, "", "", false, true)
+		c.SetCookie("token", response.Token, 3600, "", "", false, true)
+
 		c.JSON(http.StatusOK, response)
 	} else {
 		// Respond with an HTTP error if authentication fails
@@ -38,16 +43,37 @@ func login(c *gin.Context) {
 
 // Handler for the logout request
 func logout(c *gin.Context) {
-	// Obtain the POSTed username and token values
-	username := c.PostForm("username")
-	token := c.PostForm("token")
+	// Obtain the username and token from the cookies
+	username, err1 := c.Cookie("username")
+	token, err2 := c.Cookie("token")
 
-	if auth.Logout(username, token) {
-		// Respond with an HTTP success status
+	if err1 == nil && err2 == nil && auth.Logout(username, token) {
+		// Clear the cookies and
+		// respond with an HTTP success status
+		c.SetCookie("username", "", -1, "", "", false, true)
+		c.SetCookie("token", "", -1, "", "", false, true)
+
 		c.JSON(http.StatusOK, nil)
 	} else {
 		// Respond with an HTTP error
 		c.AbortWithStatus(http.StatusUnauthorized)
 	}
 
+}
+
+// Handler to serve the protected content
+func serveProtectedContent(c *gin.Context) {
+	// Obtain the username and token from the cookies
+	username, err1 := c.Cookie("username")
+	token, err2 := c.Cookie("token")
+
+	if err1 == nil && err2 == nil && auth.Authenticate(username, token) {
+		// Respond with an HTTP success status & include the
+		// content in the response
+
+		c.JSON(http.StatusOK, gin.H{"content": "This should be visible to authenticated users only."})
+	} else {
+		// Respond with an HTTP error
+		c.AbortWithStatus(http.StatusUnauthorized)
+	}
 }
